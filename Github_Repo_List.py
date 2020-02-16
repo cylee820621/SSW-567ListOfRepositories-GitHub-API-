b2f2928911387b20d31643d6daa6bda48a312409
cy <cylee820621@gmail.com>
2020-02-16 18:32:06 -0500

    fix API exceed bug

--- Github_Repo_List.py
+++ Github_Repo_List.py
@@ -10,6 +10,14 @@ Repo: Square567 Number of commits: 27
 import requests
 import json
 
+def get_Github_API(userID):
+    response = requests.get("https://api.github.com/users/{}/repos".format(userID))
+    response = response.json()
+    if response["message"] == "API rate limit exceeded for 173.230.67.55. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)":
+        print("API rate limit exceeded")
+        return None
+    return response
+
 def get_repo_list(github_api_response):
     repo_list = [repo['name'] for repo in github_api_response]
     return repo_list
@@ -25,9 +33,9 @@ def get_output_list(list_repo,list_repo_commits_number):
 
 if __name__ == "__main__":
     userID = input("enter a user ID: ")
-    response = requests.get("https://api.github.com/users/{}/repos".format(userID))
-    response = response.json()
-    list_repo = get_repo_list(response)
-    list_repo_commits_number = [get_repo_commits_number(userID,repo) for repo in list_repo]
-    list_output = get_output_list(list_repo,list_repo_commits_number)    
-    print(list_output)
+    response = get_Github_API(userID)
+    if response:
+        list_repo = get_repo_list(response)
+        list_repo_commits_number = [get_repo_commits_number(userID,repo) for repo in list_repo]
+        list_output = get_output_list(list_repo,list_repo_commits_number)    
+        print(list_output)
