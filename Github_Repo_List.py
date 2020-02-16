4a46fdc8eefba828f10f46d23da21ff19d5143a7
cy <cylee820621@gmail.com>
2020-02-16 17:50:46 -0500

    refactoring #1

--- Github_Repo_List.py
+++ Github_Repo_List.py
@@ -1,18 +1,16 @@
 #To retrieve a user's list of repositories you can use this GitHub API:
 #https://api.github.com/users/<ID>/repos
 """
+author: Chih-Yu Lee
+
 output example:
 Repo: Triangle567 Number of commits: 10
 Repo: Square567 Number of commits: 27
 """
 import requests
 import json
-userID = input("enter a user ID: ")
-response = requests.get("https://api.github.com/users/{}/repos".format(userID))
-response = response.json()
 
 def get_repo_list(github_api_response):
-    #get github repo list
     repo_list = [repo['name'] for repo in github_api_response]
     return repo_list
 
@@ -21,10 +19,15 @@ def get_repo_commits_number(userID,repo_name):
     number = len((number.json()))
     return number
 
-list_repo = get_repo_list(response)
-list_repo_commits_number = [get_repo_commits_number(userID,repo) for repo in list_repo]
+def get_output_list(list_repo,list_repo_commits_number):
+    list_output = ["Repo: {} Number of commits: {}".format(name, commits) for name, commits in zip(list_repo,list_repo_commits_number)]
+    return list_output
 
-list_output = ["Repo: {} Number of commits: {}".format(name, commits) for name, commits in zip(list_repo,list_repo_commits_number)]
-print(list_repo)
-print(list_repo_commits_number)
-print(list_output)
+if __name__ == "__main__":
+    userID = input("enter a user ID: ")
+    response = requests.get("https://api.github.com/users/{}/repos".format(userID))
+    response = response.json()
+    list_repo = get_repo_list(response)
+    list_repo_commits_number = [get_repo_commits_number(userID,repo) for repo in list_repo]
+    list_output = get_output_list(list_repo,list_repo_commits_number)    
+    print(list_output)
