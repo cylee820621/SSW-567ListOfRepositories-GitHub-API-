4e67d70fd5f8f8b4b1163e77690b7ba445dcf9cc
cy <cylee820621@gmail.com>
2020-02-26 10:23:22 -0500

    add mock test

--- test_mock_github_repo_list.py
+++ test_mock_github_repo_list.py
@@ -6,13 +6,30 @@ import json
 
 class Testgithubapi(unittest.TestCase):
     
-    @patch('requests.get')
+    @patch('Github_Repo_List.requests.get')
     def test_get_Github_API(self,mock_get):
         with open('response.json') as fopen:
             data = json.load(fopen)
         mock_get.return_value = data
         response = get_Github_API('cylee820621')
         self.assertEqual(response, data)
+
+    @patch('Github_Repo_List.requests.get')
+    def test_get_repo_list(self, mock_get):
+        with open('response.json') as fopen:
+            data = json.load(fopen)
+        mock_get.return_value = data
+        response = get_Github_API('cylee820621')
+        response = get_repo_list(response)
+        repo_list = ['SSW-533-project',
+                      'SSW-567',
+                      'SSW-567HW02a-Triangle',
+                      'SSW-567ListOfRepositories-GitHub-API-',
+                      'SSW690Project',
+                      'Univerity',
+                      'Xcodetesting']
+        self.assertEqual(response, repo_list)
+
 
 if __name__ == '__main__':
     unittest.main()
